744e0907a77f8ee129ed7aca118d60ea5eff88dd
h9896 <h123453047@gmail.com>
2018-04-23 22:09:06 +0800

    price change
    price change

--- price.py
+++ price.py
@@ -22,16 +22,28 @@ def us():
         lis.append(li)
     for i in lis:
         if i[0] == 'BTC/USD':
-           btc = 0.01070521*float(i[1])
+            btc = 0.04570521*float(i[1])
+            b_btc = (float(i[1])-5871.263)*0.04570521
+            h_btc = (float(i[1])/5871.263)
         elif i[0] == 'ETH/USD':
-            eth = 0.52222000*float(i[1])
+            eth = 0.97222000*float(i[1])
+            b_eth = (float(i[1])-584.9943)*0.97222000
+            h_eth = (float(i[1])/584.9943)
         elif i[0] == 'BCH/USD':
             bch = 0.07449677*float(i[1])
+            b_bch = (float(i[1])-3709.556)*0.07449677
+            h_bch = (float(i[1])/3709.556)
         elif i[0] == 'XRP/USD':
-            xrp = 150.000000*float(i[1])
+            xrp = 260.000000*float(i[1])
+            b_xrp = (float(i[1])-1.230573)*260.000000
+            h_xrp = (float(i[1])/1.230573)
         elif i[0] == 'ZEC/USD':
-            zec = 0.10000000*float(i[1])
+            zec = 0.62609799*float(i[1])
+            b_zec = (float(i[1])-575.7732)*0.62609799
+            h_zec = (float(i[1])/575.7732)
     total = btc+eth+bch+xrp+zec
+    b_total = b_btc+b_eth+b_bch+b_xrp+b_zec
+    
 	
     print('Total: '+str(total)+'\n')
     print('BTC: '+str(int(btc/total*100))+'%')
@@ -39,8 +51,21 @@ def us():
     print('BCH: '+str(int(bch/total*100))+'%')
     print('XRP: '+str(int(xrp/total*100))+'%')
     print('ZEC: '+str(int(zec/total*100))+'%')
+    print('Balance Total: '+str(b_total)+'\n')
+    print('Balance BTC: '+str(b_btc))
+    print('Balance BTC: '+str(int((h_btc-1)*100))+'%\n')
+    print('Balance ETH: '+str(b_eth))
+    print('Balance ETH: '+str(int((h_eth-1)*100))+'%\n')
+    print('Balance BCH: '+str(b_bch))
+    print('Balance BCH: '+str(int((h_bch-1)*100))+'%\n')
+    print('Balance XRP: '+str(b_xrp))
+    print('Balance XRP: '+str(int((h_xrp-1)*100))+'%\n')
+    print('Balance ZEC: '+str(b_zec))
+    print('Balance ZEC: '+str(int((h_zec-1)*100))+'%\n')
 
 if __name__ == '__main__':
-	while True:
-		us()
-		time.sleep(180)
+    while True:
+        ss = input("Search:")
+        if ss =="Y":
+            us()
+            #time.sleep(180)
